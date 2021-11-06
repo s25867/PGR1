92193359ecf047816d620c0370498592dcce6d8b
s25867 <s25867@pjwstk.edu.pl>
2021-11-06 22:06:55 +0100

    pora dnia koniec

--- include/s03-Time.h
+++ include/s03-Time.h
@@ -8,6 +8,10 @@ struct time1{
 	int sekunda;
 	time1(int, int, int);
 	auto to_string() const -> std::string;
+	auto next_hour() -> void;
+	auto next_minute() -> void;
+	auto next_second() -> void;
+	auto tod() const -> std::string;
 };
 
 
--- src/s03-Time.cpp
+++ src/s03-Time.cpp
@@ -1,4 +1,3 @@
-#include <s03-Time2.h>
 #include <s03-Time.h>
 #include <string>
 #include <iostream>
@@ -7,10 +6,21 @@ auto time1::to_string() const -> std::string
 {
 	auto out = std::ostringstream{};
 		std::string x;
+		std::string y;
+		std::string z;
 	if (godzina<10)
 		{x = "0";}
 	else {x = "";}
-	out << this->godzina << x << ":" << this->minuta << y << ":" << this->sekunda << z << "\n";
+
+        if (minuta<10)
+                {y = "0";}
+        else {y = "";}
+
+        if (sekunda<10)
+                {z = "0";}
+        else {z = "";}
+
+	out << x << this->godzina << ":" << y << this->minuta << ":" << z << this->sekunda << "\n";
 
 	return out.str();
 }
@@ -22,19 +32,59 @@ time1::time1(int godzina, int minuta, int sekunda)
         this->sekunda = sekunda;
 }
 
-// +1 godzina
-//time1::time1(int godzina, int minuta, int sekunda)
-//{
-//	this->godzina = 1 + godzina;
-//	this->minuta = 1 + minuta;
-//	this->sekunda = 1 + sekunda;
-//}
+auto time1::next_hour() -> void
+{
+	if (godzina==23)
+		{godzina = 0;}
+	else
+		godzina++;
+}
+
+auto time1::next_minute() -> void
+{
+	if (minuta==59)
+		{minuta = 0;
+		godzina++;}
+	else
+		minuta++;
+}
+
+auto time1::next_second() -> void
+{
+	if (sekunda==59)
+		{sekunda = 0;
+		minuta++;}
+	else
+		sekunda++;
+}
+
+auto time1::tod() const -> std::string
+{
+        auto out = std::ostringstream{};
+		if (godzina < 6)
+	{out << "night\n";}
+		else if (godzina < 12)
+	{out << "morning\n";}
+		else if (godzina < 16)
+	{out << "afternoon\n";}
+		else if (godzina < 20)
+	{out << "evening\n";}
+		else
+	{out << "evening\n";}
+return out.str();
+}
+
+
 auto main() -> int
 {
-	auto s = time1{9, 50, 12};
+	auto s = time1{7, 13, 0};
 
 	std::cout << s.to_string();
-
+	s.next_hour();
+	s.next_minute();
+	s.next_second();
+	std::cout << s.to_string();
+	std::cout << s.tod();
 }
 
 
