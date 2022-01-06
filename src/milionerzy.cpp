e6f2ecc513213a4bf611a02002c6beae39e92312
s25867 <91957102+s25867@users.noreply.github.com>
2022-01-06 22:56:42 +0100

    Losowe odp

--- src/milionerzy.cpp
+++ src/milionerzy.cpp
@@ -1,20 +1,30 @@
-#include <question.h>
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int Guess;
 int Total;
 
-struct QuestionValues
-{
+bool game_over() {
+	cout << "Twoje punkty spadly ponizej 0, to koniec gry" << endl;
+	return 0;
+}
+
+// Question Class
+class Question {
+public:
 	string Question_Text;
-	string Answer_1;
-	string Answer_2;
-	string Answer_3;
-	string Answer_4;
+	string Correct;
 	int Correct_Answer;
 	int Question_Score;
+	vector <string> Wrong;
+	// Function to ask questions
+	void askQuestion();
+};
 
 // Driver code
 int main()
@@ -34,105 +44,101 @@ int main()
 	}
 
 
-	while (Total >= 0)
-	{
-	Question::Ask();
-	}
-	else
-	{
-	Question::check()
+
+	// Objects of Question Class
+	Question q1;
+	q1.Question_Text = "Jakiego koloru jest zolty maluch: ";
+	srand((unsigned)time(0));
+	q1.Correct_Answer = (rand() % 4);
+	q1.Correct = "Zoltego";
+	q1.Wrong.push_back("Czerwonego");
+	q1.Wrong.push_back("Zielonego");
+	q1.Wrong.push_back("Niebieskiego");
+	q1.Wrong.push_back("Czarnego");
+	q1.Question_Score = 10;
+
+	Question q2;
+	q2.Question_Text = "Jakiej rośliny dodaje się aby uzyskać herbatę earl gray : ";
+	srand((unsigned)time(0));
+	q2.Correct_Answer = (rand() % 4);
+	q2.Correct = "Bergamotki";
+	q2.Wrong.push_back("Róży");
+	q2.Wrong.push_back("Mięty");
+	q2.Wrong.push_back("Niebieskiego");
+	q2.Wrong.push_back("Wanilii");
+	q2.Question_Score = 10;
+
+	vector<Question>vQuestion = { q1, q2 };
+
+	vQuestion.at(0).askQuestion();
 
 
-
-	// Objects of Question 
 	// pytanie, odp, odp, odp, odp, numer popr odp, wartosc
-	q1.setValues("Jakiego koloru jest żółty maluch: ", "Czerwonego",
-				"Zielonego", "Żółtego",
-				"Niebieskiego", 3, 10);
-	q2.setValues("Jakiej rośliny dodaje się aby uzyskać herbatę earl gray : ", "Bergamotki",
-				"Róży", "Mięty",
-				"Wanilii", 1, 10);
-	q3.setValues("Ile nóg ma stonoga : ", "Dziesięć",
-				"Cztery", "Dziewiędziesiąt",
-				"Sto", 1, 10);
-	q4.setValues("Jak ma na imię prezydent polski : ", "Duda",
-				"Kamil", "Andrzej",
-				"Jarosław", 3, 10);
-	q5.setValues("Ile lat ma doda : ", "37",
-				"50", "29",
-				"42", 1, 10);
-	q6.setValues("Które z tych państw nie leży w Europie : ", "Polska",
-				"Finlandia", "Rosja",
-				"Australia", 4, 10);
-	q7.setValues("Łacińska nazwa na kota domowego : ", "cattus domesticus",
-				"domesticus cattus", "canis cattus",
-				"domesticus canis", 2, 10);
-	q8.setValues("Ile godzin ma doba : ", "12",
-				"168", "24",
-				"26", 3, 10);
-	q9.setValues("W którym roku po raz pierwszy wyemitowano Milionerów w Polsce : ", "1999",
-				"1994", "2001",
-				"2004", 1, 10);
-	q10.setValues("Jak się nazywa stolica Polski : ", "Warszawa",
-				"Łódź", "Poznań",
-				"Gdańsk", 1, 10);
-}
+	//q2.setValues("Jakiej rośliny dodaje się aby uzyskać herbatę earl gray : ", "Bergamotki", "Róży", "Mięty", "Wanilii", 1, 10);
+	//q3.setValues("Ile nóg ma stonoga : ", "Dziesięć", "Cztery", "Dziewiędziesiąt", "Sto", 1, 10);
+	//q4.setValues("Jak ma na imię prezydent polski : ", "Duda", "Kamil", "Andrzej", "Jarosław", 3, 10);
+	//q5.setValues("Ile lat ma doda : ", "37", "50", "29", "42", 1, 10);
+	//q6.setValues("Które z tych państw nie leży w Europie : ", "Polska", "Finlandia", "Rosja", "Australia", 4, 10);
+	//q7.setValues("Łacińska nazwa na kota domowego : ", "cattus domesticus", "domesticus cattus", "canis cattus", "domesticus canis", 2, 10);
+	//q8.setValues("Ile godzin ma doba : ", "12", "168", "24", "26", 3, 10);
+	//q9.setValues("W którym roku po raz pierwszy wyemitowano Milionerów w Polsce : ", "1999", "1994", "2001", "2004", 1, 10);
+//	q10.setValues("Jak się nazywa stolica Polski : ", "Warszawa","Łódź", "Poznań", "Gdańsk", 1, 10);
 
-// q question, a1 answer 1, ca correct answer, pv points value
-void Question::setValues(string q, string a1, string a2, string a3, string a4, int ca, int pv)
-{
-	Question_Text = q;
-	Answer_1 = a1;
-	Answer_2 = a2;
-	Answer_3 = a3;
-	Answer_4 = a4;
-	Correct_Answer = ca;
-	Question_Score = pv;
-}
+	q2.askQuestion();
 
-//Check if won/lost quiz
-int Question::check()
-{
-        if (Total >= 100) {
-                cout << "Gratulacje zdałeś quiz" << endl;
-        	break;
-}
 
-        else
-        if (Total < 0) {
-                cout << "Twoje punkty wynoszą " << Total << ". To koniec, przegrałeś!" << endl;
-        	break;
-}
-return 0;
-}
+	// Display the total score
+	cout << "Twój wynik = " << Total
+		<< " na 100" << endl;
 
+	// Display the results
+
+	// If the player pass the quiz
+	if (Total >= 70) {
+		cout << "Gratulacje zdałeś quiz" << endl;
+	}
+
+	// Otherwise
+	else {
+		cout << "Niestety nie zdałeś."
+			<< endl;
+		cout << "Powodzenia następnym razem."
+			<< endl;
+	}
+	return 0;
+}
 
 // Function to ask questions
-void Question::Ask()
+void Question::askQuestion()
 {
 	cout << endl;
-
+	int nums[4] = { 0, 1, 2, 3 };
+	random_shuffle(nums, nums + 4);
+	srand((unsigned)time(0));
 	// Print the questions
 	cout << Question_Text << endl;
-	cout << "1. " << Answer_1 << endl;
-	cout << "2. " << Answer_2 << endl;
-	cout << "3. " << Answer_3 << endl;
-	cout << "4. " << Answer_4 << endl;
+	for (int index = 0; index < 4; index++) {
+		if (Correct_Answer == index)
+			cout << index + 1 << " " << Correct << endl;
+		else
+				cout << index + 1 << " " << Wrong.at(nums[index]) << endl;
+	}
+
 	cout << endl;
 
 	// Display the answer
-	cout << "Jaka jest twoja odpowiedź (cyfrą)"
+	cout << "Jaka jest twoja odpowiedz (cyfra)"
 		<< endl;
 	cin >> Guess;
 
 	// If the answer is correct
-	if (Guess == Correct_Answer) {
+	if (Guess == Correct_Answer+1) {
 		cout << endl;
 		cout << "Prawda !" << endl;
 
 		// Update the correct score
 		Total = Total + Question_Score;
-		cout << "Zobyłeś " << Question_Score << " punktów!" << endl;
+		cout << "Wynik = " << Question_Score << " na " << Question_Score << "!" << endl;
 		cout << endl;
 	}
 
@@ -140,12 +146,11 @@ void Question::Ask()
 	else {
 		cout << endl;
 		cout << "Fałsz !" << endl;
-		cout << "Tracisz " << Question_Score << " punktów!" << endl;
-		cout << "Poprawna odpowiedź = "
-			<< Correct_Answer
-			<< "." << endl;
-		Total = Total - Question_Score;
+		cout << "Wynik = 0 " << " na " << Question_Score << "!" << endl;
+		cout << "Poprawna odpowiedź = " << Correct_Answer << "." << endl;
 		cout << endl;
+		Total = Total - Question_Score;
+		if (Total < 0)
+			game_over();
 	}
 }
-
